2996fd0c714d5ad0472d9b78b2b7891008b541ce
rishabh-live <rishabh12536@gmail.com>
2020-10-19 17:15:22 +0530

    Added Title and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {} from '../components/main.css';
 import {} from '../components/localLibs/w3.min.css';
+import { title } from 'process';
+import HEAD from 'next/head';
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps }
+    return (
+        <>
+        <HEAD>
+            <title>Rishabh-live | Rishabh | rishabh.live</title>
+            <link rel="icon" href="/main-ico.png"/>
+        </HEAD>
+    <Component {...pageProps }
     />
+    </>);
 }
