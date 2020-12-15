974442d0c44b3fc45bcdb09c018734d7e93c94b4
rishabh-live <rishabh12536@gmail.com>
2020-12-15 20:14:14 +0530

    Added Google Analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,11 +4,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 
 
-export default function App({ Component, pageProps }) {
-  return (
-    <html lang="en-US">
-      
-      <Component {...pageProps} />
-    </html>
-  );
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
+
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return <Component {...pageProps} />
 }
+
+export default App
