eb30bd481a61715b13ffc6fd615e37678dfc8e5c
rishabh-live <rishabh12536@gmail.com>
2020-12-15 19:28:26 +0530

    Blog Article Card Hard Coded for Desktop

--- pages/blogs/index.jsx
+++ pages/blogs/index.jsx
@@ -1,4 +1,5 @@
 import HEAD from "next/head";
+import LINK from "next/link";
 export default function Blogs() {
   return (
     <>
@@ -11,9 +12,45 @@ export default function Blogs() {
         <center>
           <nav>
             <div className="banner">
-              <img src="/assets/images/Banner-Classy.png" alt="rishabh live classy style banner"/>
+              <img
+                src="/assets/images/Banner-Classy.png"
+                alt="rishabh live classy style banner"
+              />
             </div>
           </nav>
+        </center>
+      </section>
+      <section>
+        <center>
+          <div className="mainCard">
+            <div className="blogCard">
+              <img
+                src="/assets/images/Banner-Linkedin.png"
+                alt="Banner Classy"
+                className="mainImage"
+              />
+              <br />
+              <br />
+              <label htmlFor="title" id="title">
+                An Awesome Title
+              </label>
+              <br />
+              <label htmlFor="shorts" id="short">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem
+                ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum
+                dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor
+                sit amet consectetur adipisicing elit.
+              </label>
+              <br />
+              <center>
+                <LINK href="/blogs/an-awesone-article" as="/blogs/an-awesone-article">
+                  <a>
+                    <button id="readMore">Read More</button>
+                  </a>
+                </LINK>
+              </center>
+            </div>
+          </div>
         </center>
       </section>
     </>
